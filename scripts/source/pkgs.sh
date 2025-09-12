53017d45670a7303e7e18d11999f8851c9100487
sclosaf <sclosaf@gmail.com>
2025-09-12 22:37:53 +0000

    update

---
+++ scripts/source/pkgs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export packages=(
+    "sway"
+    #"neovim"
+    #"waybar"
+    #"yazi"
+    #"wofi"
+    #"btop"
+    #"fastfetch"
+    #"kitty"
+    #"alacritty"
+    #"fish"
+    #"bash"
+    #"zsh"
+    #"ttf-jetbrains-mono-nerd"
+    #"brightnessctl"
+)
