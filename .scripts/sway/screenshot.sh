6d71b24ab0a0d0aef34087bc91b15bf63cd167c9
sclosaf <sclosaf@gmail.com>
2025-09-14 12:55:49 +0000

    update

---
+++ .scripts/sway/screenshot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p ~/Pictures/screenshots
+if [ $1 == "window" ]; then
+    DIM=$(swaymsg -t get_tree | jq -r '.. | select(.focused?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"')
+elif [ $1 == "select" ]; then 
+    DIM="$(slurp)
+fi
+
+if [ "$DIM" != "selection cancelled" ]; then 
+    grim -g "$DIM" - | wl-copy -t image/png
+    wl-paste > ~/Pictures/screenshots/screenshot_$(date +'%Y-%m-%s_%H-%M-%S.png')
+fi
