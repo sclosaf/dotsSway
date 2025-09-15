017c887842056cbc00a1fc318ac2202e636691a5
sclosaf <sclosaf@gmail.com>
2025-09-15 07:31:02 +0000

    update

--- scripts/source/fonts.sh
+++ scripts/source/fonts.sh
@@ -2,4 +2,7 @@
 
 declare -A fontList=(
     ["JetBrains Mono Nerd"]="ttf-jetbrains-mono-nerd"
+    ["Firacode Nerd"]="ttf-firacode-nerd"
+    ["Victor Mono Nerd"]="ttf-victor-mono-nerd"
+    ["Roboto Mono Nerd"]="ttf-roboto-mono-nerd"
 )
